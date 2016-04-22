b49dd57530bc3bb0b266203eddf859597d380523
weyden-herrera <weyden.herrera@gmail.com>
2016-04-22 10:52:01 +0800

    first commit

---
+++ src/main/java/com/dest/configuration/DestApplication.java
@@ -0,0 +1,12 @@
+package com.dest.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestApplication {
+	public static void main(String[] args){
+		SpringApplication.run(DestApplication.class, args);
+	}
+	
+}
